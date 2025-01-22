e8a051e1330952b0385ff64132a68c86f4750ecc
NgenoMark <kipchumbamark48@gmail.com>
2025-01-22 12:27:33 +0300

    Modified files

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
+import 'dart:async';
+import 'pages/login_page.dart'; // Import your login page
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Project Management App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(), // Set SplashScreen as the first screen
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () { // Change 3 to the duration you want
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Change this to match your splash screen design
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/splash_logo.jpg" , width: 200,),
+            SizedBox(height: 20),
+            CircularProgressIndicator(), // Loading animation
+          ],
+        ),
+      ),
     );
   }
 }
--- frontend/lib/pages/register_page.dart
+++ frontend/lib/pages/register_page.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/pages/login_page.dart';
-import 'project_vendor_register_page.dart'; // Import registration pages for each user type
-import 'project_executor_register_page.dart';
-import 'project_manager_register_page.dart';
+// import 'project_vendor_register_page.dart'; // Import registration pages for each user type
+// import 'project_executor_register_page.dart';
+// import 'project_manager_register_page.dart';
 import 'fieldman_register_page.dart';
 
 class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Select Account Type')),
+      appBar: AppBar(title: Text('Select Account')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
-              'Select account type',
+              'Select account ',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
@@ -54,47 +54,47 @@ class RegisterPage extends StatelessWidget {
             Column(
               children: <Widget>[
                 // Project Vendor button
-                _buildRegisterButton(
-                  context,
-                  'Register as Project Vendor',
-                  'For companies that post projects',
-                  () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ProjectVendorRegisterPage()),
-                    );
-                  },
-                ),
-                SizedBox(height: 16),
+                // _buildRegisterButton(
+                //   context,
+                //   'Register as Project Vendor',
+                //   'For companies that post projects',
+                //   () {
+                //     Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (context) => ProjectVendorRegisterPage()),
+                //     );
+                //   },
+                // ),
+                // SizedBox(height: 16),
                 // Project Executor button
-                _buildRegisterButton(
-                  context,
-                  'Register as Project Executor',
-                  'For companies offering services',
-                  () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ProjectExecutorRegisterPage()),
-                    );
-                  },
-                ),
-                SizedBox(height: 16),
+                // _buildRegisterButton(
+                //   context,
+                //   'Register as Project Executor',
+                //   'For companies offering services',
+                //   () {
+                //     Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (context) => ProjectExecutorRegisterPage()),
+                //     );
+                //   },
+                // ),
+                // SizedBox(height: 16),
                 // Project Manager button
-                _buildRegisterButton(
-                  context,
-                  'Register as Project Manager',
-                  'Managers within a project executor company',
-                  () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ProjectManagerRegisterPage()),
-                    );
-                  },
-                ),
-                SizedBox(height: 16),
+                // _buildRegisterButton(
+                //   context,
+                //   'Register as Project Manager',
+                //   'Managers within a project executor company',
+                //   () {
+                //     Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (context) => ProjectManagerRegisterPage()),
+                //     );
+                //   },
+                // ),
+                // SizedBox(height: 16),
                 // Fieldman button
                 _buildRegisterButton(
                   context,
