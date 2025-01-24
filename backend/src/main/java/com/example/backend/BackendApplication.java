6b8dc4d9edbea97a75b0a4dcf2b504c6cfd49cf1
NgenoMark <kipchumbamark48@gmail.com>
2025-01-24 12:37:13 +0300

    Modified and added properties

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 //import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 
 //@SpringBootApplication
 @SpringBootApplication(scanBasePackages = "com.example.backend")
+@EnableJpaRepositories(basePackages = "com.example.backend.api.repository")
 
 public class BackendApplication {
 
@@ -32,7 +34,7 @@ public class BackendApplication {
 
     private static void testDatabaseConnection(JdbcTemplate jdbcTemplate) {
         try {
-            jdbcTemplate.execute("SELECT 1 FROM projectvendor");
+            jdbcTemplate.execute("SELECT 1 FROM PBCATCOL");
             System.out.println("Connection successful");
         } catch (Exception e){
             System.err.println(e.getMessage());
