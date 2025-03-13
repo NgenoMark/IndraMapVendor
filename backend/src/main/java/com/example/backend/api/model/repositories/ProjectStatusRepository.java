395baa994602a2a0325ed9c848fecec3820e6bc5
NgenoMark <kipchumbamark48@gmail.com>
2025-03-13 15:47:22 +0300

    modified repository extension

--- backend/src/main/java/com/example/backend/api/model/repositories/ProjectStatusRepository.java
+++ backend/src/main/java/com/example/backend/api/model/repositories/ProjectStatusRepository.java
@@ -1,4 +1,10 @@
 package com.example.backend.api.model.repositories;
 
-public interface ProjectStatusRepository {
+import com.example.backend.api.model.ProjectStatus;
+import com.example.backend.api.model.ProjectStatusId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectStatusRepository extends JpaRepository<ProjectStatus, ProjectStatusId> {
+
+
 }
