3bc57dc4ee55668c49ec301a54a4f729924c7636
NgenoMark <kipchumbamark48@gmail.com>
2025-03-24 12:01:26 +0300

    Updated the details sectons and made them more presentable

--- frontend/lib/pages/project_details_page.dart
+++ frontend/lib/pages/project_details_page.dart
@@ -6,19 +6,18 @@ import 'package:intl/intl.dart';
 class ProjectDetailsPage extends StatefulWidget {
   final dynamic projectData;
 
-  const ProjectDetailsPage({Key? key, required this.projectData})
-      : super(key: key);
+  const ProjectDetailsPage({Key? key, required this.projectData}) : super(key: key);
 
   @override
   _ProjectDetailsPageState createState() => _ProjectDetailsPageState();
 }
 
-class _ProjectDetailsPageState extends State<ProjectDetailsPage>
-    with SingleTickerProviderStateMixin {
+class _ProjectDetailsPageState extends State<ProjectDetailsPage> with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   List<dynamic>? surveyData;
   List<dynamic>? paymentData;
+  List<dynamic>? materialData;
 
   @override
   void initState() {
@@ -26,6 +25,7 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage>
     _tabController = TabController(length: 3, vsync: this);
     fetchSurveyData();
     fetchPaymentData();
+    fetchMaterialData();
   }
 
   @override
@@ -35,15 +35,12 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage>
   }
 
   void fetchSurveyData() async {
-    final url =
-        Uri.parse('http://localhost:8081/survey/searchSurvey/MAPXYZ678901');
-
+    final url = Uri.parse('http://localhost:8081/survey/searchSurvey/MAPXYZ678901');
     try {
       final response = await http.get(url);
-
       if (response.statusCode == 200) {
         setState(() {
-          surveyData = json.decode(response.body); // List<dynamic>
+          surveyData = json.decode(response.body);
         });
       } else {
         print('Failed to load survey data: ${response.statusCode}');
@@ -55,21 +52,33 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage>
 
   void fetchPaymentData() async {
     final url = Uri.parse('http://localhost:8081/payment/numMap/MAPUPL001503');
-
-    //'http://localhost:8081/payment/numMap/${widget.projectData['mapNo']}');
-
     try {
       final response = await http.get(url);
-
       if (response.statusCode == 200) {
         setState(() {
-          paymentData = json.decode(response.body); // List<dynamic>
+          paymentData = json.decode(response.body);
         });
       } else {
         print('Failed to load payment data: ${response.statusCode}');
       }
     } catch (e) {
       print('Error fetching payment data: $e');
+    }
+  }
+
+  void fetchMaterialData() async {
+    final url = Uri.parse('http://localhost:8081/materials/mapNo/MAPXYZ678901');
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        setState(() {
+          materialData = json.decode(response.body);
+        });
+      } else {
+        print('Failed to load material data: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching material data: $e');
     }
   }
 
@@ -103,20 +112,27 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage>
     return SingleChildScrollView(
       padding: EdgeInsets.all(16.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          _buildDetailRow('Application No', data['applicationNo']),
-          _buildDetailRow('Map Vendor ID', data['mapVendorId']),
-          _buildDetailRow('Map No', data['mapNo']),
-          _buildDetailRow('Customer Name', data['customerName']),
-          _buildDetailRow('Customer Address', data['customerAddress']),
-          _buildDetailRow('Customer Telephone', data['customerTelephone']),
-          _buildDetailRow('City', data['city']),
-          _buildDetailRow('District', data['district']),
-          _buildDetailRow('Zone', data['zone']),
-          _buildDetailRow('Completion Status', data['completionStatus']),
-          _buildDetailRow('Survey Status', data['surveyStatus']),
-          _buildDetailRow('Assigned to', data['assignedToId']),
+          _buildDetailCard('Project Details', [
+            _buildDetailRow('Application No', data['applicationNo']),
+            _buildDetailRow('Map Vendor ID', data['mapVendorId']),
+            _buildDetailRow('Map No', data['mapNo']),
+          ]),
+          _buildDetailCard('Customer Details', [
+            _buildDetailRow('Customer Name', data['customerName']),
+            _buildDetailRow('Customer Address', data['customerAddress']),
+            _buildDetailRow('Customer Telephone', data['customerTelephone']),
+          ]),
+          _buildDetailCard('Location Details', [
+            _buildDetailRow('City', data['city']),
+            _buildDetailRow('District', data['district']),
+            _buildDetailRow('Zone', data['zone']),
+          ]),
+          _buildDetailCard('Status Details', [
+            _buildDetailRow('Completion Status', data['completionStatus']),
+            _buildDetailRow('Survey Status', data['surveyStatus']),
+            _buildDetailRow('Assigned to', data['assignedToId']),
+          ]),
         ],
       ),
     );
@@ -129,20 +145,21 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage>
       return Center(child: Text('No survey data available.'));
     }
 
-    final survey = surveyData![
-        0]; // Assuming you want to display only the first survey entry
+    final survey = surveyData![0];
 
     return SingleChildScrollView(
       padding: EdgeInsets.all(16.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          _buildDetailRow('Map Number', survey['mapNumber']),
-          _buildDetailRow('Map Vendor', survey['vendorId']),
-          _buildDetailRow('Phase Type', survey['tipFase']),
-          _buildDetailRow('Last Altered', formatDate(survey['fActual'])),
-          _buildDetailRow('Survey Id', survey['idSurveyData'].toString()),
-          _buildDetailRow('Remarks', survey['remarks']),
+          _buildDetailCard('Survey Details', [
+            _buildDetailRow('Map Number', survey['mapNumber']),
+            _buildDetailRow('Map Vendor', survey['vendorId']),
+            _buildDetailRow('Phase Type', survey['tipFase']),
+            _buildDetailRow('Last Altered', formatDate(survey['fActual'])),
+            _buildDetailRow('Survey Id', survey['idSurveyData'].toString()),
+            _buildDetailRow('Remarks', survey['remarks']),
+          ]),
+          _buildMaterialsUsedSection(), // Materials inside survey tab
         ],
       ),
     );
@@ -155,66 +172,70 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage>
       return Center(child: Text('No payment data available.'));
     }
 
-    final payment = paymentData![0];
-
     return SingleChildScrollView(
-      padding: EdgeInsets.all(16.0),
+      padding: EdgeInsets.all(16),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          _buildDetailRow('Map Number', payment['numMap']),
-          _buildDetailRow('Map Vendor', payment['coMapVend']),
-          // _buildDetailRow('Application Number', survey['tipFase']),
-
-          _buildDetailRow('Account Payer ID', payment['accountId'].toString()),
-          _buildDetailRow('Total Amount', payment['impTotRec'].toString()),
-          _buildDetailRow('Payment Date', formatDate(payment['fActual'])),
-          _buildDetailRow('Status', payment['estPago']),
-        ],
+        children: paymentData!.map((payment) {
+          return _buildDetailCard('Payment Details', [
+            _buildDetailRow('Map Number', payment['numMap']),
+            _buildDetailRow('Map Vendor', payment['coMapVend']),
+            _buildDetailRow('Account Payer ID', payment['accountId'].toString()),
+            _buildDetailRow('Total Amount', payment['impTotRec'].toString()),
+            _buildDetailRow('Payment Date', formatDate(payment['fActual'])),
+            _buildDetailRow('Status', payment['estPago']),
+          ]);
+        }).toList(),
       ),
     );
-  
+  }
 
-    // return ListView.builder(
-    //   padding: EdgeInsets.all(16.0),
-    //   itemCount: paymentData!.length,
-    //   itemBuilder: (context, index) {
-    //     final payment = paymentData![index];
-    //     return Card(
-    //       margin: EdgeInsets.symmetric(vertical: 8.0),
-    //       child: ListTile(
-    //         title: Text('Payment ID: ${payment['paymentId'] ?? 'N/A'}'),
-    //         subtitle: Column(
-    //           crossAxisAlignment: CrossAxisAlignment.start,
-    //           children: [
-    //             Text('Amount: ${payment['amount'] ?? 'N/A'}'),
-    //             Text('Paid By: ${payment['paidBy'] ?? 'N/A'}'),
-    //             Text('Date: ${formatDate(payment['date'])}'),
-    //           ],
-    //         ),
-    //       ),
-    //     );
-    //   },
-    // );
+  Widget _buildMaterialsUsedSection() {
+    if (materialData == null) {
+      return Center(child: CircularProgressIndicator());
+    } else if (materialData!.isEmpty) {
+      return Center(child: Text('No material data available.'));
+    }
+
+    return _buildDetailCard('Materials Used', materialData!.map((material) {
+      return _buildDetailRow(
+        '${material['material']}',
+        '',
+      );
+    }).toList());
+  }
+
+  Widget _buildDetailCard(String title, List<Widget> children) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 16),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            ...children,
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _buildDetailRow(String label, String? value) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Text('$label: ${value ?? 'N/A'}', style: TextStyle(fontSize: 16)),
+      padding: EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(label, style: TextStyle(fontWeight: FontWeight.bold)),
+          Text(value ?? 'N/A'),
+        ],
+      ),
     );
   }
 
-  String formatDate(String? dateString) {
-    if (dateString == null || dateString.isEmpty) {
-      return 'N/A';
-    }
-    try {
-      DateTime parsedDate = DateTime.parse(dateString);
-      return DateFormat('yyyy-MM-dd HH:mm:ss').format(parsedDate);
-    } catch (e) {
-      print("Error parsing date: $e");
-      return 'Invalid Date';
-    }
+  String formatDate(String? date) {
+    if (date == null) return 'N/A';
+    return DateFormat.yMMMMd().format(DateTime.parse(date));
   }
 }
